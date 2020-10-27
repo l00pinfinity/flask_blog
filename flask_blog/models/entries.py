84cdf2fcc5f971256e3c52c09a75d7091865a316
l00pinfinity <loopinfinity@criptext.com>
2020-10-27 18:13:51 +0300

    one-to-one correspondents, btw models and views

--- flask_blog/models/entries.py
+++ flask_blog/models/entries.py
@@ -4,16 +4,16 @@ from flask_blog import db
 from datetime import datetime
 
 class Entry(db.Model):
-    __tablename__='entries'
-    id = db.Column(db.Interger, primary_key = True)
-    title = db.Column(db.String(50), unique = True)
+    __tablename__ = 'entries'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True)
     text = db.Column(db.Text)
-    created_at = db.Column(db.Date-Time)
+    created_at = db.Column(db.DateTime)
 
-        def __init__(self, title = None, text = None):
-            self.title = title
-            self.text = text
-            self.created_at = datetime.utc-now()
+    def __init__(self, title=None, text=None):
+        self.title = title
+        self.text = text
+        self.created_at = datetime.utcnow()
 
     def __repr__(self):
-        return'<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
+        return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
