2874ba16d0178abb63d2f8684e22fdaddface5c9
l00pinfinity <loopinfinity@criptext.com>
2020-10-30 12:57:04 +0300

    added new post

--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -3,13 +3,6 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
 
-@app.route('/')
-def show_entries():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/index.html')
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -23,6 +16,7 @@ def login():
             flash('Login successful')
             return redirect(url_for('show_entries'))
     return render_template('login.html')
+
 
 
 @app.route('/logout')
